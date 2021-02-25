c540b58dd8ab168addfaa09b59b29d904d8dc3d5
Pedro Provenzano <pedroprov0@gmail.com>
2021-02-24 21:00:33 -0300

    cambios en public de API

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// Script De HUD
+const botonNoticias = document.getElementById("titulo-noticias");
+const botonMensajes = document.getElementById("titulo-mensajes");
+
+// Escenas
+const noticias = document.getElementById("noticias");
+const mensajes = document.getElementById("mensajes");
+
+botonNoticias.addEventListener("click", () => {
+  noticias.style.display = "flex";
+  mensajes.style.display = "none";
+});
+
+botonMensajes.addEventListener("click", () => {
+  noticias.style.display = "none";
+  mensajes.style.display = "flex";
+});
