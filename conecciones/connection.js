486e32f4e964b685aefb9ea85817e5e422f4ee8c
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-03 22:16:33 -0300

    pos solut timezone

--- conecciones/connection.js
+++ conecciones/connection.js
@@ -3,6 +3,17 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   `mysql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORTSQL}/${process.env.DATABASE_NAME}`,
   {
+    dialectOptions: {
+      useUTC: false, //for reading from database
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
     timezone: "-03:00",
   }
 );
