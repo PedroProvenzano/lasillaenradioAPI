a8545241fc326fef4feeb80bda1bc71eba61e8de
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-16 17:19:16 -0300

    agregada tabla de imagen

--- conecciones/migraciones.js
+++ conecciones/migraciones.js
@@ -1,7 +1,9 @@
 const Mensaje = require("../modelos/Mensaje");
 const Noticia = require("../modelos/Noticia");
+const Imagen = require("../modelos/Imagen");
 
 (async () => {
   await Mensaje.sync();
   await Noticia.sync();
+  await Imagen.sync();
 })();
