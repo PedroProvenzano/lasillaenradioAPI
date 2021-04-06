72d8ebd31bbbab6a628a2b349fe784be9ac9034b
Pedro Provenzano <pedroprov0@gmail.com>
2021-04-05 21:18:21 -0300

    fix 1

--- conexiones/migraciones.js
+++ conexiones/migraciones.js
@@ -2,10 +2,12 @@ const Mensaje = require("../modelos/Mensaje");
 const Noticia = require("../modelos/Noticia");
 const Imagen = require("../modelos/Imagen");
 const Trivia = require("../modelos/Trivia");
+const Visita = require("../modelos/Visita");
 
 (async () => {
   await Mensaje.sync();
   await Noticia.sync();
   await Imagen.sync();
   await Trivia.sync();
+  await Visita.sync();
 })();
